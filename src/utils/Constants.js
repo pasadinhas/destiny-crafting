927337e45dfeed0fe626e8c80e779b86a613b44f
pasadinhas <pasadinhas.miguel@gmail.com>
2022-11-16 21:22:45 +0000

    feat: add deployment scripts

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,7 +1,7 @@
 const Constants = Object.freeze({
-    CLIENT_ID: '41755',
-    CLIENT_SECRET: 'oGmn0q3MH.SudEiKhMpphwr4Pj-khgOG.E3Ru9W7h58',
-    API_KEY: '4ffdd2ed48c24718a00c35f30e0c9223',
+    CLIENT_ID: process.env.REACT_APP_BUNGIE_OAUTH_CLIENT_ID || '41755',
+    CLIENT_SECRET: process.env.REACT_APP_BUNGIE_OAUTH_CLIENT_SECRET || 'oGmn0q3MH.SudEiKhMpphwr4Pj-khgOG.E3Ru9W7h58',
+    API_KEY: process.env.REACT_APP_BUNGIE_OAUTH_API_KEY || '4ffdd2ed48c24718a00c35f30e0c9223',
     ACCESS_TOKEN_KEY: 'access_token'
 })
 
